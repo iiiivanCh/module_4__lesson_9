ae6206fe255e65a3d36213edf9cf9cc1f9cf6b62
iiiivanCh <iiiivanchm@yandex.ru>
2022-06-14 01:47:10 +0300

    fix bug eslint

--- cart.js
+++ cart.js
@@ -1,16 +1,16 @@
-"use strict"
+'use strict';
 
 const cart = {
   items: [],
   count: 0,
-  get totalPrice()  {
+  get totalPrice() {
     return this.totalPriceCalc;
   },
   set totalPrice(val) {
     if (this.totalPriceCalc !== val) {
       this.totalPrice = this.totalPriceCalc;
-      console.log("Извините, нельзя");
-    } 
+      console.log('Извините, нельзя');
+    }
   },
   // totalPrice: 0,
 
@@ -24,14 +24,20 @@ const cart = {
     this.calculateItemPrice();
   },
   calculateItemPrice() {
-    this.totalPriceCalc = this.items.reduce((sum, {price, count}) => sum + (price * count), 0);
+    this.totalPriceCalc = this.items.reduce((sum, {
+      price,
+      count,
+    }) => sum +
+      (price * count), 0);
   },
   increaseCount() {
-    this.count = this.items.reduce((sum, {count}) => sum + count, 0);
+    this.count = this.items.reduce((sum, {
+      count,
+    }) => sum + count, 0);
   },
   print() {
     const basket = JSON.stringify(this.items);
-    console.log(basket + '\n' + 'totalPrice: ' + this.totalPrice);
+    console.log(`${basket}\ntotalPrice: ${this.totalPrice}`);
   },
   clear() {
     this.items.length = 0;
@@ -41,18 +47,18 @@ const cart = {
 };
 
 
-cart.add("сапоги", 5000, 10);
-cart.add("пальто", 5000, 10);
+cart.add('сапоги', 5000, 10);
+cart.add('пальто', 5000, 10);
 cart.print();
 cart.totalPrice = 4;
 console.log(cart.totalPrice);
-cart.add("шапка", 5000, 10);
+cart.add('шапка', 5000, 10);
 console.log(cart);
 cart.print();
 cart.clear();
 cart.print();
 console.log(cart);
-cart.add("пальто", 5000, 10);
+cart.add('пальто', 5000, 10);
 console.log(cart.totalPrice);
 cart.clear();
 console.log(cart);
